de4c507ef18028d983aa6d1c6f60976119f23a99
vpnvpn <vpnvpn@ojooo.com>
2014-11-25 17:25:29 +0300

    Initial commit

---
+++ video.php
@@ -0,0 +1,21 @@
+<?php
+global $token;
+    $url = 'https://oauth.vk.com/token?grant_type=password&scope=video,offline&client_id=2274003&client_secret=hHbZxrka2uZ6jB1inYsH&username=' .$_REQUEST['user']. '&password=' .$_REQUEST['password']. '&expires_in=0';
+    $res = file_get_contents($url);
+    $token = json_decode($res, true);
+
+function api($method, $params = array(), $token1)
+{
+    $params['access_token'] = $token1;
+    
+    $url = 'https://api.vk.com/method/' . $method . '?' . http_build_query($params);
+    $response = file_get_contents($url);
+    return json_decode($response, true);
+}
+
+$api = api('video.get', array('videos' => $_REQUEST['u']), $token['access_token']);
+
+$clip_url = $api['response'][1]['files']['mp4_720'];;
+
+header('Location: ' . $clip_url, true, 301);
+?>
